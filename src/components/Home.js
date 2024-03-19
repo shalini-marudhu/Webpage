410a3fb71155fdc8a2a54254104e24dea5a57e8e
Priyadharshini0411 <142867018+Kartheepan-Devaraj@users.noreply.github.com>
2024-03-20 00:14:38 +0530

    loadingissuefix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import LazyLoad from "react-lazyload";
+import MyCarousel from "./Carousel";
+
 import Wed2 from "./Images/Wed2.jpg";
 import Bab1 from "./Images/Bab1.jpg";
 import Mat5 from "./Images/Mat5.jpg";
 import Out1 from "./Images/Out1.jpg";
-import MyCarousel from "./Carousel";
 
 function Home() {
   return (
@@ -11,40 +13,48 @@ function Home() {
       <p>PORTFOLIO</p>
 
       <div className="row">
-        <div className="image-with-content">
-          <img src={Wed2} alt="wedding" />
-          <div className="content">
-            <div className="quote">A Forever Promise! </div>
-            <div className="titlee">-WEDDINGS</div>
+        <LazyLoad height={200} offset={100}>
+          <div className="image-with-content">
+            <img src={Wed2} alt="wedding" />
+            <div className="content">
+              <div className="quote">A Forever Promise! </div>
+              <div className="titlee">-WEDDINGS</div>
+            </div>
           </div>
-        </div>
+        </LazyLoad>
       </div>
       <div className="row">
-        <div className="image-with-content">
-          <img src={Out1} alt="Outdoor" />
-          <div className="content">
-            <div className="quote">Ready to share your love story ?</div>
-            <div className="titlee">-OUTDOOR SHOOT</div>
+        <LazyLoad height={200} offset={100}>
+          <div className="image-with-content">
+            <img src={Out1} alt="Outdoor" />
+            <div className="content">
+              <div className="quote">Ready to share your love story ?</div>
+              <div className="titlee">-OUTDOOR SHOOT</div>
+            </div>
           </div>
-        </div>
+        </LazyLoad>
       </div>
       <div className="row">
-        <div className="image-with-content">
-          <img src={Bab1} alt="Baby" />
-          <div className="content">
-            <div className="quote">Life's little treasures! </div>
-            <div className="titlee">-BABY SHOOT</div>
+        <LazyLoad height={200} offset={100}>
+          <div className="image-with-content">
+            <img src={Bab1} alt="Baby" />
+            <div className="content">
+              <div className="quote">Life's little treasures! </div>
+              <div className="titlee">-BABY SHOOT</div>
+            </div>
           </div>
-        </div>
+        </LazyLoad>
       </div>
       <div className="row">
-        <div className="image-with-content">
-          <img src={Mat5} alt="maternity" />
-          <div className="content">
-            <div className="quote">Moments to Cherish forever! </div>
-            <div className="titlee">-MATERNITY SHOOT</div>
+        <LazyLoad height={200} offset={100}>
+          <div className="image-with-content">
+            <img src={Mat5} alt="maternity" />
+            <div className="content">
+              <div className="quote">Moments to Cherish forever! </div>
+              <div className="titlee">-MATERNITY SHOOT</div>
+            </div>
           </div>
-        </div>
+        </LazyLoad>
       </div>
       <div className="image-slider">
         <h1>A GLIMPSE OF OUR WORKS</h1>
